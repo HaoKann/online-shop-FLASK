62b2e21c846d44a9839bb6df349a41f20b59fedd
HaoKann <baizhanovdamir@mail.ru>
2025-03-16 13:45:33 +0500

    Installed modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
+from app.config import Config
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
 
+bootstrap = Bootstrap5(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-
-
-
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Войдите в аккаунт для получения доступа'
 
 
 
