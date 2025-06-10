aae6c69bfa162317455003430aec3b6e4f669e4e
HaoKann <baizhanovdamir@mail.ru>
2025-06-10 19:57:23 +0500

    add admin search for all products, upgrade catalog pages design

--- app/controllers/search_controller.py
+++ app/controllers/search_controller.py
@@ -15,3 +15,18 @@ def search():
             result = result.filter(Product.name.ilike(f'%{word}%'))
         result = result.all()    
     return render_template('search/search.html', result=result, q=q)
+
+
+@app.route('/admin/search', methods = ['GET','POST'])
+def admin_search():
+    q = request.args.get('q')
+    if not q:
+        result = []
+        flash('Неправильно введеный запрос', 'danger')
+    else:
+        words = q.split(' ')
+        result = db.session.query(Product).filter(Product.name.ilike(f'%{words[0]}%'))
+        for word in words[1:]:
+            result = result.filter(Product.name.ilike(f'%{word}%'))
+        result = result.all()    
+    return render_template('admin/admin_search.html', result=result, q=q)
