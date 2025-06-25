ac9a6f918ba7d3fe5f05d88c4ff6ef46d6f7448e
HaoKann <baizhanovdamir@mail.ru>
2025-06-25 14:37:52 +0500

    start making about us page

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -1,9 +1,14 @@
 from app import app
 from flask import render_template
 from flask_login import login_required
-
+from app.models.about import Milestone
 
 @app.route('/')
-@login_required
 def main():
     return render_template('main.html')
+
+
+@app.route('/about')
+def about():
+    milestones = Milestone.query.order_by(Milestone.year.asc()).all()
+    return render_template('main_screen/about.html', milestones=milestones)
