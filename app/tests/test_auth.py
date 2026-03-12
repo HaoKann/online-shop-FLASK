e600c78327e0cb90c7c4665bdefa4854f13ebf21
HaoKann <baizhanovdamir@mail.ru>
2026-03-12 13:56:57 +0500

    fix auth tests, test 404 error

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,23 +1,52 @@
 from app.models.user import User
+from datetime import date
 from app import db
 
 def test_login_success(client, app):
     """Тест: Проверяем, что пользователь может успешно войти на сайт."""
 
     # 1. ПОДГОТОВКА: Создаем пользователя в тестовой базе
-    user = User(nickname='test_ninja', email='ninja@example.com')
+    user = User(
+        name='Тестовый Ниндзя', 
+        nickname='test_ninja', 
+        email='ninja@example.com', 
+        date_of_birth=date(2000, 1, 1))
     user.set_password('12345678')
     db.session.add(user)
     db.session.commit()
 
     # 2. ДЕЙСТВИЕ: Отправляем данные формы на сервер (POST-запрос)
     response = client.post('/login', data={
-        'email': 'ninja@example.com',
-        'password': '12345678'
-    }, follow_redirects=True) 
+        'nickname': 'ninja@example.com',
+        'new_password': '12345678'
+    }, follow_redirects=False) 
     # follow_redirects=True означает: "если сервер скажет перенаправить на главную страницу после входа - переходи"
 
-    # 3. ПРОВЕРКИ:
-    assert response.status_code == 200
-    assert b'test_ninja' in response.data
+    # 3. ПРОВЕРКА:
+    # Если вход успешен, Flask вернет код 302 (Redirect) на главную или в профиль!
+    assert response.status_code == 302
 
+
+def test_login_failure(client, app):
+    """Тест: Проверяем, что с неправильным паролем войти нельзя."""
+
+    # 1. Создаем пользователя
+    user = User(
+        name='Плохой Хакер',
+        nickname="hacker", 
+        email="hacker@example.com",
+        date_of_birth=date(1995, 5, 5))
+    user.set_password('correct_password')
+    db.session.add(user)
+    db.session.commit()
+
+    # 2. Пытаемся войти с НЕПРАВИЛЬНЫМ паролем
+    response = client.post('/login', data={
+        'nickname': 'hacker@example.com',
+        'new_password': 'wrong_password_123'
+    }, follow_redirects=True)
+
+    # 3. Проверяем, что нас НЕ пустило (имя hacker не должно появиться как у авторизованного)
+    # И мы всё еще видим кнопку или текст, связанный с ошибкой входа
+    assert response.status_code == 200
+    assert b'hacker' not in response.data
--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -9,3 +9,12 @@ def test_hompage_loads(client):
 
     # Проверяем, что на странице есть наше название (например, "PerfectPC" или "Почему выбирают нас")
     assert b"PerfectPC" in response.data or b"Perfect PC" in response.data
+
+def test_404_error_page(client):
+    """Тест: Проверяем, что сайт правильно обрабатывает несуществующие адреса."""
+
+    # Виртуальный браузер пытается зайти на несуществующую страницу 
+    response = client.get('/page_does_not_exist')
+
+    # Сервер должен честно признаться, что такой страницы нет (код 404)
+    assert response.status_code == 404
